70fadcc9f072fdc2510c1ec755b5e024a4fb5261
Lee <leebee1234@hotmail.co.uk>
2017-05-08 19:06:49 +0100

    J

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match('newquay.html');
+    })
+  );
+});
