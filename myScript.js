176b01980e35d4158b2c0b1ee2588558fe04c018
Lee <leebee1234@hotmail.co.uk>
2017-05-09 01:31:57 +0100

    j

--- myScript.js
+++ myScript.js
@@ -1,28 +1,28 @@
-let options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
+function geoFindMe() {
+  var output = document.getElementById("out");
 
-const output = document.getElementById("myLocation");
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
 
-function success(pos) {
-	
-	let latitude  = pos.coords.latitude;
-                        let longitude = pos.coords.longitude;
-	let accuracy = pos.coords.accuracy;
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
-    output.innerHTML = `Latitude is ${latitude} and Longitude is ${longitude}. More or less ${accuracy} metres.`;
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
-    const img = new Image();
+    var img = new Image();
     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
     output.appendChild(img);
+  }
 
-};
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
 
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-};
+  output.innerHTML = "<p>Locating…</p>";
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+  navigator.geolocation.getCurrentPosition(success, error);
+}
